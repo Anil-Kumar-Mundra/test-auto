613fe5aab687607dcd72fd4e76f3132060d84274
Anil Kuamr Mundra <amundra@teksystems.com>
2019-02-12 22:13:43 +0530

    good commit

---
+++ reports/trail.js
@@ -0,0 +1,17 @@
+
+let results =
+{
+    "testsuite1" : [
+        {
+            "testcase1" : [
+                {
+                "stepId": 101,
+                "stepType": "Application",
+                "stepMethod" : "setValue",
+                "stepInput"  : " Text Message ",
+                "locatorType" : "xpath",
+                "locatorValue" : "//*[@id='user-message']" }
+                    ]        },
+
+    ]
+}
